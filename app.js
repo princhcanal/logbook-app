248509745999fbdae19af1a6be5ffaa3e7223e8a
Princh Canal <princhcanalxd@gmail.com>
2020-06-02 13:23:45 +0800

    changed title

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ let express = require("express"),
 	app = express(),
 	bodyParser = require("body-parser"),
 	methodOverride = require("method-override"),
-	path = require("path");
-// liveReload = require("livereload"),
-// liveReloadServer = liveReload.createServer(),
-// connectLiveReload = require("connect-livereload");
+	path = require("path"),
+	liveReload = require("livereload"),
+	liveReloadServer = liveReload.createServer(),
+	connectLiveReload = require("connect-livereload");
 
 app.set("port", process.env.PORT || 3000);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname + "/public")));
-// liveReloadServer.watch(path.join(__dirname + "/public"));
+liveReloadServer.watch(path.join(__dirname + "/public"));
 app.use(methodOverride("_method"));
-// app.use(connectLiveReload());
+app.use(connectLiveReload());
 
 app.get("/", (req, res) => {
 	res.render("index");
