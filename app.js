3a55f071a2ecb3428bc516671e4752c3648b62cb
Princh Canal <princhcanalxd@gmail.com>
2020-06-08 00:35:35 +0800

    added db

--- app.js
+++ app.js
@@ -24,12 +24,12 @@ app.use(flash());
 app.use(methodOverride("_method"));
 
 let db = "";
-// if (app.get("port") === 3000) {
-// 	db = "mongodb://localhost:27017/logbook_app";
-// } else {
-// 	db =
-// 		"mongodb://princhcanal:logbook@cluster0-shard-00-00-onyef.mongodb.net:27017,cluster0-shard-00-01-onyef.mongodb.net:27017,cluster0-shard-00-02-onyef.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
-// }
+if (app.get("port") === 3000) {
+	db = "mongodb://localhost:27017/logbook_app";
+} else {
+	db =
+		"mongodb://princhcanal:logbook@cluster0-shard-00-00-onyef.mongodb.net:27017,cluster0-shard-00-01-onyef.mongodb.net:27017,cluster0-shard-00-02-onyef.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
+}
 mongoose.connect(db, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
