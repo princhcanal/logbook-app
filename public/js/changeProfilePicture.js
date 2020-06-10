e71028058c6c56e02d74faeacc64af8f37c89982
Princh Canal <princhcanalxd@gmail.com>
2020-06-10 23:31:55 +0800

    improved profile picture

--- public/js/changeProfilePicture.js
+++ public/js/changeProfilePicture.js
@@ -1,15 +1,21 @@
 let profilePictures = document.querySelectorAll('.profile-image');
+let editProfile = document.querySelector('#editProfile');
 let fileInput = document.querySelector('#profileImage');
 let editProfileForm = document.querySelector('#editProfileForm');
+let loader = document.querySelector('.loader');
 let url = '/logbook/profile/edit/picture';
 
 fileInput.addEventListener('change', (e) => {
     let formData = new FormData(editProfileForm);
+    loader.style.display = 'block';
+    editProfile.style.display = 'none';
     axios.put(url, formData).then(response => {
         let imageUrl = `/uploads/${formData.get('profilePicture').name}`;
         profilePictures.forEach(profilePicture => {
             profilePicture.setAttribute('src', imageUrl);
-        })
+        });
+        loader.style.display = 'none';
+        editProfile.style.display = 'inline-block';
     }).catch(err => {
         console.log(err);
     });
