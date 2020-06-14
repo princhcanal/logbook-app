1b123e77875316f781f0aa2a6dd13ab4da751723
Princh Canal <princhcanalxd@gmail.com>
2020-06-14 17:29:46 +0800

    changed file name

--- utilities/upload.js
+++ utilities/upload.js
@@ -4,7 +4,7 @@ let storage = multer.diskStorage({
         cb(null, './uploads/')
     },
     filename: function (req, file, cb) {
-        cb(null, new Date().toISOString() + file.originalname);
+        cb(null, file.originalname);
     }
 });
 let fileFilter = (req, file, cb) => {
